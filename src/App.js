96ecdbd073177795018047f6b955d61e16bc0a92
alperenustun <alperen.ustun39@gmail.com>
2023-05-22 12:37:46 +0300

    ortadaki yazı degistirildi & deneme 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Git & Github Kursuna Hoşgeldiniz.
+          Git Github Öğreniyorum -Alperen
+          Deneme 1
         </p>
         <a
           className="App-link"
